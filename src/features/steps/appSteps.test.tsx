e0e41621d8976fb365a3b2c0a56e33d1a506c927
Campbell <campbell@dataforce.com.au>
2020-06-10 13:51:04 +1000

    Actually add a failing test

--- src/features/steps/appSteps.test.tsx
+++ src/features/steps/appSteps.test.tsx
@@ -31,7 +31,7 @@ defineFeature(feature, test => {
         })
         
         then('value should be in test', () => {
-            expect(1).toEqual(1)
+            expect(1).toEqual(2)
         })
     })
 })
